4733eda98c19bc33f6392d1083e26168b592d357
Josh Pelkey <joshpelkey@gmail.com>
2016-12-07 10:17:24 -0500

    fix navbar active class

--- cfbets/bets/views.py
+++ cfbets/bets/views.py
@@ -8,16 +8,16 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 @login_required(login_url='/login/')
 def my_bets(request):
-	return render(request, 'bets/base_my_bets.html')
+	return render(request, 'bets/base_my_bets.html', {'nbar': 'my_bets'})
 
 @login_required(login_url='/login/')
 def open_bets(request):
-	return render(request, 'bets/base_open_bets.html')
+	return render(request, 'bets/base_open_bets.html', {'nbar': 'open_bets'})
 
 @login_required(login_url='/login/')
 def all_bets(request):
-	return render(request, 'bets/base_all_bets.html')
+	return render(request, 'bets/base_all_bets.html', {'nbar': 'all_bets'})
 
 @staff_member_required(login_url='/')
 def admin_bets(request):
-	return render(request, 'bets/base_admin_bets.html')
+	return render(request, 'bets/base_admin_bets.html', {'nbar': 'admin_bets'})
