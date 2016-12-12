f258b8639cbb5d2ebc8b2698a3ffd1de6e66de4c
Josh Pelkey <joshpelkey@gmail.com>
2016-12-12 16:22:47 -0500

    update place bet form style

--- cfbets/bets/forms.py
+++ cfbets/bets/forms.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class PlaceBetsForm(forms.Form):
 	qty_allowed = forms.IntegerField(label='Qty Allowed', initial=2, min_value=1, max_value=10)
-	bet = forms.CharField(label='Bet', max_length=500, widget=forms.TextInput(attrs={'placeholder': 'E.g. Clemson -48.5 over USCjr', 'size': '50'}))
+	bet = forms.CharField(label='Bet', max_length=500, widget=forms.TextInput(attrs={'placeholder': 'E.g. Clemson -48.5 over USCjr'}))
 	bet_amount = forms.IntegerField(label='Bet Amount (USD)', initial=5, min_value=1, max_value=20)
 	bet_expiration_date = forms.DateTimeField(label='Expiration Date/Time', help_text='Bet will no longer be available for others to accept after this datetime.')
 
