90a82ea6bb257d9711f019d97497f9423e939705
Josh Pelkey <joshpelkey@gmail.com>
2018-01-28 17:58:33 -0500

    password reset broken

--- cfbets/cfbets/urls.py
+++ cfbets/cfbets/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'base_login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^sign_up/$', sign_up),
-	url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'registration/password_reset_form.html'},  name='password_reset'),
+    url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'registration/password_reset_form.html'},  name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
