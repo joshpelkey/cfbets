a9d6547b541a93bf1c158b6913daaf18caa22817
Josh Pelkey <joshpelkey@gmail.com>
2019-04-03 16:11:26 -0400

    updates for slack on appeng

--- cfbets/bets/views.py
+++ cfbets/bets/views.py
@@ -1,5 +1,11 @@
 import json
 import requests
+import requests_toolbelt.adapters.appengine
+
+# Use the App Engine Requests adapter. This makes sure that Requests uses
+# URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
